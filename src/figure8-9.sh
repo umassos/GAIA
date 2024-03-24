54e2116de9f6eead54c24717e12f02f4c3c81753
Walid <eng_walid_ashraf@hotmail.com>
2024-03-24 16:23:18 +0200

    Initial Commit

--- src/figure8-9.sh
+++ src/figure8-9.sh
@@ -8,14 +8,11 @@ python3 src/run.py --scheduling-policy carbon --carbon-policy lowest -w 6x24
 # Lowest Carbon Window
 python3 src/run.py --scheduling-policy carbon --carbon-policy waiting -w 6x24
 
-# Lowest Carbon Window (Oracle)
-python3 src/run.py --scheduling-policy carbon --carbon-policy oracle -w 6x24
-
 # Carbon Saving per Waiting Time
 python3 src/run.py --scheduling-policy carbon --carbon-policy cst_average -w 6x24
 
-# Carbon Saving per Waiting Time (Oracle)
-python3 src/run.py --scheduling-policy carbon --carbon-policy cst_oracle -w 6x24
+# Ecovisor
+python3 src/run.py --scheduling-policy suspend-resume-threshold --carbon-policy oracle -w 6x24
 
 # Wait AWhile
 python3 src/run.py --scheduling-policy suspend-resume --carbon-policy oracle -w 6x24
